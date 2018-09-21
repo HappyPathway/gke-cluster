ac15ee2b1ec93a269aa8dcb4d1201d2331b73bff
Dave Arnold <darnold@hashicorp.com>
2018-09-21 14:26:31 -0700

    updating main.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "cluster_zone" {
+    
+}
+
+variable "cluster_env" {
+  
+}
+
+variable "cluster_name" {
+
+}
+
+
+variable "vault_gcp_credentials_path" {
+    default = "secret/credentials/gcp"
+}
