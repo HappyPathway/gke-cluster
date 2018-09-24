4db8813aea633ccab7e2f38b06431de99293ba83
Dave Arnold <darnold@hashicorp.com>
2018-09-24 12:00:11 -0700

    remvong all else

--- update.tf
+++ update.tf
@@ -3,16 +3,3 @@ resource "null_resource" "timestamp" {
       command = "date +%s | /usr/bin/env python2.7 -c 'import sys, json; print json.dumps(dict(timestamp=sys.stdin.read().strip()))' > /tmp/date.json" 
   }
 }
-
-data "external" "json_date" {
-    depends_on = [
-        "null_resource.timestamp"
-    ]
-    program = ["cat", "/tmp/date.json"]
-}
-
-resource "null_resource" "force_update" {
-  provisioner "local-exec" {
-      command = "echo ${data.external.json_date.result}"
-  }
-}
