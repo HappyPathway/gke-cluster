6f8956d7528f24bbc06897b1624284845239e90b
Dave Arnold <darnold@hashicorp.com>
2018-09-24 10:11:17 -0700

    fixing outputs

--- main.tf
+++ main.tf
@@ -55,21 +55,3 @@ resource "google_container_cluster" "primary" {
   }
 }
 
-# The following outputs allow authentication and connectivity to the GKE Cluster.
-output "client_certificate" {
-  value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
-}
-
-output "client_key" {
-  value = "${google_container_cluster.primary.master_auth.0.client_key}"
-}
-
-output "cluster_ca_certificate" {
-  value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
-}
-
-
-output "endpoint" {
-  value       = "${google_container_cluster.primary.endpoint}"
-  description = "The IP address of this cluster's Kubernetes master"
-}
