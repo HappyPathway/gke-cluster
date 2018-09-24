77e7ebe3ffc64cac8c883e255d515eecbfbba13c
Dave Arnold <darnold@hashicorp.com>
2018-09-24 11:24:51 -0700

    fixing stuff

--- update.tf
+++ update.tf
@@ -1,3 +1,14 @@
+variable "timestamp_cmd" {
+    type = "list"
+    description = "Command for timestamp"
+    default = ["date", 
+               "|", 
+               "/usr/bin/env", 
+               "python2.7", 
+               "-c", 
+               "import sys, json; print json.dumps(dict(timestamp=sys.stdin.read().strip()))"] 
+}
+
 data "external" "date" {
   program = "${var.timestamp_cmd}"
 }
